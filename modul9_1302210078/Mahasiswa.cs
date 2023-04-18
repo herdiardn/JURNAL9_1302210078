ba08edda735b9960c17302de0d5ea99995e7b675
herdiardn <121550473+herdiardn@users.noreply.github.com>
2023-04-18 16:46:44 +0700

    menambahkan API mahasiswa

---
+++ modul9_1302210078/Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿
+public class Mahasiswa
+{
+    public string name { get; set; }
+    public string nim { get; set; }
+    public List<string> course { get; set; }
+    public int year { get; set; }
+
+    public Mahasiswa(string name, string nim, List<string> course, int year)
+    {
+        this.name = name;
+        this.nim = nim;
+        this.course = course;
+        this.year = year;
+    }
+}
